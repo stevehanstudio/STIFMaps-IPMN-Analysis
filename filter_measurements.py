0c8bc088e563a2bc9d384394786cccd56e2515bd
Steve Han <15229911+stevehanstudio@users.noreply.github.com>
2025-04-07 00:10:15 -0700

    Modify filter_measurements to loop through BASE_NAMES

--- filter_measurements.py
+++ filter_measurements.py
@@ -1,8 +1,11 @@
 from globals_and_helpers import (
+    BASE_NAMES,
     filter_measurements,
 )
 
+for base_name in BASE_NAMES:
+    filter_measurements(base_name)
 # filter_measurements("27620")
-filter_measurements("4601")
+# filter_measurements("4601")
 # filter_measurements("7002")
 
