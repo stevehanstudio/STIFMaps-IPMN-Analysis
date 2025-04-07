d115225cdfddea5d0ac7c7e54777565aac8c5dee
Steve Han <15229911+stevehanstudio@users.noreply.github.com>
2025-04-06 23:01:06 -0700

    Fixed scale_annotation to use width and height original IPMN images as input instead of resized images

--- scale_annotations.py
+++ scale_annotations.py
@@ -1,6 +1,9 @@
 from globals_and_helpers import BASE_NAMES, gen_report, scale_annotations
 
 for base_name in BASE_NAMES:
-    scale_annotations(base_name, resized=True)
-# scale_annotations("4601")
+    try:
+        scale_annotations(base_name, resized=True)
+    except Exception as e:
+        print(f"Error processing {base_name}: {e}")
+# scale_annotations("4601", resized=True)
 # gen_report("7002")
